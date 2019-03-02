cd1a3593737ba1287b6903cd05f9dd7136a9a84b
junforjun <kyuno1984@gmail.com>
2019-03-02 23:29:11 +0900

    #What i did today? vue cli3設置、Componentの理解、文法勉強 bootStrap, jquery 適用
    ヘッダ作成
    fontawesome의 glyphicon 適用（Bootstrap4はglyphiconを支援しない）

---
+++ my-blog/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+      <div class="d-flex flex-column h-100">
+    <header>
+  <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">Jun's Blog</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#"><i class="fas fa-home"></i> <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" ><i class="fas fa-edit"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" ><i class="fas fa-cog"></i></a>
+        </li>
+      </ul>
+      <form class="form-inline mt-2 mt-md-0 ml-auto p-2">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+
+    
+  </nav>
+</header>
+
+<!-- Begin page content -->
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    
+  </div>
+</main>
+
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    
+  }
+}
+</script>
